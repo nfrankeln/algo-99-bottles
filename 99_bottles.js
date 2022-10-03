c3c9d4ef9f76317dd09acafd341ddf5a2606b6f6
Adam Cahan <adam.cahan@gmail.com>
2022-10-03 16:15:38 -0500

    Update 99_bottles.js

--- 99_bottles.js
+++ 99_bottles.js
@@ -1,9 +1,5 @@
 function bottleSong() {
-  console.log('hello world!!!!')
-
-  let numBottles = 99
-  console.log(numBottles + ' bottles of beer on the wall!!!')
+  // code goes here
 };
 
-console.log('another message')
 bottleSong();
