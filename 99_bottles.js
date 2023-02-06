d2afc8b4ea68b11a7756ba3c65429fa94b50c0b7
nfrankeln <ntnyfranky@gmail.com>
2023-02-06 10:26:51 -0600

    added normal/recursive solution to 99 bottle

--- 99_bottles.js
+++ 99_bottles.js
@@ -1,5 +1,34 @@
+// While loop answer 
 function bottleSong() {
-  // code goes here
+  let bottles = 9
+  while (bottles > 1){
+    console.log(`${bottles} bottles of beer on the wall, ${bottles} bottles of beer.
+      Take one down and pass it around, ${bottles-1} bottles of beer on the wall`)
+    bottles -=1
+  }
+  console.log(`1 bottle of beer on the wall, 1 bottle of beer.
+    Take one down and pass it around, no more bottles of beer on the wall.
+No more bottles of beer on the wall, no more bottles of beer.
+Go to the store and buy some more, 99 bottles of beer on the wall.`)
 };
 
-bottleSong();
+//recursive answer
+function bottleSongRecursive(n){
+  
+  if ( n > 1 )
+  {
+    console.log(`${n} bottles of beer on the wall, ${n} bottles of beer.
+    Take one down and pass it around, ${n-1} bottles of beer on the wall`)
+    return bottleSongRecursive(n-1)
+  }
+
+  else { 
+    console.log(
+      `1 bottle of beer on the wall, 1 bottle of beer.
+    Take one down and pass it around, no more bottles of beer on the wall.
+No more bottles of beer on the wall, no more bottles of beer.
+Go to the store and buy some more, 99 bottles of beer on the wall.`
+    )
+  }
+}
+
